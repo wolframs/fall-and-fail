c4d307763098b3e4841623d2ee179e230e828207
Wolfram S <wolframs91@gmail.com>
2020-09-04 11:41:47 +0200

    Camera Smoothing

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Wolfram:
+/// Stellt Funktionalität bereit, um Kamera FoV laufend zu manipulieren und ggf. auf aktueller Position basiert
+/// zu verlangsamen und zu beschleunigen (smoothing).
+/// </summary>
 public class Cam : MonoBehaviour
 {
     public Camera scriptCam = Camera.current;
     public bool active = true;
+    public bool smoothen = true;
+    public float smoothingAddend = 0.1f;
     public float stepFactor = 1;
     public float stepper = 1;
     public float timer = 0.3f;
@@ -13,6 +21,7 @@ public class Cam : MonoBehaviour
     public float lowerLimit = 2;
     private float currFoV;
     private bool zoomIn = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,26 +37,80 @@ public class Cam : MonoBehaviour
             yield return new WaitForSeconds(timeOut);
 
             if (zoomIn && (currFoV > upperLimit))
-            {
                 zoomIn = false;
-            }
             if (!zoomIn && (currFoV < lowerLimit))
-            {
                 zoomIn = true;
-            }
 
             switch (zoomIn)
             {
                 case true:
-                    currFoV = currFoV + stepper * stepFactor;
+                    currFoV = ChangeFoV(currFoV, Direction.ZoomIn);
                     break;
                 case false:
-                    currFoV = currFoV - stepper * stepFactor;
+                    currFoV = ChangeFoV(currFoV, Direction.ZoomOut);
                     break;
             }
 
             // Update cam fov
             scriptCam.fieldOfView = currFoV;
         }
+    }
+
+    private float ChangeFoV(float currFoV, Direction direction)
+    {
+        float _stepper = stepper;
+
+        if (smoothen)
+        {
+            if (IsCloserTo(currFoV, upperLimit, lowerLimit) == 1 )
+            {
+                // Näher am oberen Limit
+                _stepper += smoothingAddend * DistanceToMaxFoV(currFoV, upperLimit);
+            }
+            else
+            {
+                // Näher am unteren Limit
+                _stepper += smoothingAddend * DistanceToMaxFoV(currFoV, lowerLimit);
+            }
+        }
+
+        switch(direction)
+        {
+            case Direction.ZoomIn:
+                currFoV = currFoV + _stepper * stepFactor;
+                break;
+            case Direction.ZoomOut:
+                currFoV = currFoV - _stepper * stepFactor;
+                break;
+        }
+        
+        return currFoV;
+    }
+
+    private enum Direction
+    {
+        ZoomIn,
+        ZoomOut
+    }
+
+    private int IsCloserTo(float x, float compareValue1, float compareValue2)
+    {
+        if (
+            (Mathf.Abs(compareValue1 - x))
+            <
+            (Mathf.Abs(compareValue2 - x))
+            )
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+
+    private float DistanceToMaxFoV(float x, float targetMaxDistance)
+    {
+        return (Mathf.Abs(x - targetMaxDistance));
     }
 }

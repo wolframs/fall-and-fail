05d0892853705d8b3184f3b4da7c370b28c746b6
Wolfram S <wolframs91@gmail.com>
2020-09-04 13:00:22 +0200

    committy committy <3

---
+++ Assets/Scripts/SceneFade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SceneFade : MonoBehaviour
+{
+    public Canvas parentCanvas;
+    private Image imageToFade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageToFade = parentCanvas.GetComponent<Image>();
+        imageToFade.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

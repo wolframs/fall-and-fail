75c72e615d01fff11ea7cfe951fe6a3ae069c8ad
jderenbach <jderenbach@protakt.de>
2020-09-11 09:58:50 +0200

    Right and Left
    right and left scaling of Player controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,13 +35,30 @@ public class Player : MonoBehaviour
        
         // Hier wird bei jedem Frame entsprechend dem Keyboardinput die neue Position berechnet
         float xInput = Input.GetAxis("Horizontal");
+        bool goesleft = false;
+        if (xInput != 0)
+        {
+            bool xgoesleft = goesleft;
+            if (xInput < 0 && !goesleft)
+            {
+                goesleft = true;
+            }
+            else
+            {
+                goesleft = false;
+            }
+
+            if (goesleft != xgoesleft)
+            {
+                transform.localScale = new Vector3((transform.localScale.x * (-1)), transform.localScale.y, transform.localScale.z);
+            }
+        }
         if (Input.GetButtonDown("Jump") && grounded)
         {
-            rb2.AddForce(new Vector2(0f, 500f));
+            rb2.AddForce(new Vector2(0f, 200f));
             grounded = false;
         }
         float newpositionX = transform.position.x + xInput * Time.deltaTime * faktor;
-
         transform.position = new Vector3(newpositionX, transform.position.y, 0);
     }
 

0bd599e6e81e8aa42eb78892b899d7af9cc0df9c
jderenbach <jderenbach@protakt.de>
2020-09-18 11:48:15 +0200

    Coins einsammeln ermöglicht

---
+++ Assets/CoinCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollection : MonoBehaviour
+{
+    public GameObject gameobject;
+    public CircleCollider2D colli;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Destroy(gameobject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

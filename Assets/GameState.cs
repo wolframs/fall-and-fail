cd2d12f01cb24647da7c38405abba4af9b17014e
Wolfram S <wolframs91@gmail.com>
2020-10-23 14:34:15 +0200

    ziemlisch guuude fortshritte

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameState : MonoBehaviour
 {
@@ -11,15 +14,35 @@ public class GameState : MonoBehaviour
     public static bool jumpInProgress;
     public static bool slashInProgress;
 
+    public static UnityEngine.UI.Image HPFiller = null;
+    public static UnityEngine.UI.Image StaminaFiller = null;
+
     public static float jumpForceY = 200;
-    public static int playerHP = 100;
-    public static int playerStamina = 100;
+    public static int playerHP {
+        set {
+            HPFiller.fillAmount = (1 / value);
+        }
+    }
+    public static int playerStamina {
+        set
+        {
+            StaminaFiller.fillAmount = (1 / value);
+        }
+    }
 
     private void Awake()
     {
         // Hier erstellen wir ggf. die öffentliche statische Instanz der Klasse, wenn das Script irgendwo in einem GameObject eingebunden ist
         if (instance == null)
+        {
             instance = this;
+            HPFiller = GameObject.Find("HPFiller").GetComponent<UnityEngine.UI.Image>();
+            StaminaFiller = GameObject.Find("StaminaFiller").GetComponent<UnityEngine.UI.Image>();
+
+            playerHP = 100;
+            playerStamina = 100;
+        }
+            
         // Ggf. verhindern, dass die Instanzen mehrfach erstellt werden, wenn das Script mehr als einmal in einer Szene eingebunden ist
         else if (instance != this)
             Destroy(gameObject);

bbd10a2569c00e66e8df16882656b67ff938d424
Wolfram S <wolframs91@gmail.com>
2020-10-23 10:00:15 +0200

    Mandatory Commit b4 Pull

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+    // Da eine "MovoBehaviour" derived class nicht static sein darf, ermöglichen wir hier die Erstellung einer öffentlichen statischen Instanz
     public static GameState instance = null;
 
     public static bool playerGrounded;
@@ -12,11 +13,14 @@ public class GameState : MonoBehaviour
 
     private void Awake()
     {
+        // Hier erstellen wir ggf. die öffentliche statische Instanz der Klasse, wenn das Script irgendwo in einem GameObject eingebunden ist
         if (instance == null)
             instance = this;
+        // Ggf. verhindern, dass die Instanzen mehrfach erstellt werden, wenn das Script mehr als einmal in einer Szene eingebunden ist
         else if (instance != this)
             Destroy(gameObject);
 
+        // Das hier ist halt wichtig, stand so im Internetz, war uns jetzt nicht wichtig genug, um das zu googlen
         DontDestroyOnLoad(gameObject);
     }
 }

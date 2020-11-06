6df13c9cb988a5a1007a51aa78008575b7a1339f
jderenbach <jderenbach@protakt.de>
2020-11-06 10:29:55 +0100

    Weapon.cs

---
+++ Assets/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+
+    
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+         animator = this.GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D colli)
+    {
+        if (animator.GetBool("Attacks"))
+        {
+            Debug.Log("Treffer : " + colli.gameObject.name);
+            Destroy(colli.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
